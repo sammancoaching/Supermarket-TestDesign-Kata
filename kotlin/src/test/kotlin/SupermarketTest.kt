9c7127ccd46bf1041893232b0c86705c0fdd066d
emily <emily@bacheconsulting.com>
2025-09-29 15:44:47 +0200

    sample solution in kotlin

--- kotlin/src/test/kotlin/SupermarketTest.kt
+++ kotlin/src/test/kotlin/SupermarketTest.kt
@@ -1,82 +1,69 @@
 
 import dojo.supermarket.model.*
+import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 
 class SupermarketTest {
 
+    private val catalog: SupermarketCatalog = FakeCatalog()
+    private val cart = ShoppingCart()
+    private val toothbrush = Product("toothbrush", ProductUnit.EACH)
+    private val rice = Product("rice", ProductUnit.EACH)
+    private val cherryTomatoes = Product("cherry Tomato box", ProductUnit.EACH)
+    private val apples = Product("apples", ProductUnit.KILO)
+    private val teller: Teller = Teller(catalog)
+
+    @BeforeEach
+    fun setup() {
+        catalog.addProduct(toothbrush, 0.99)
+        catalog.addProduct(rice, 2.99)
+        catalog.addProduct(toothbrush, 0.99)
+        catalog.addProduct(cherryTomatoes, 0.69)
+        catalog.addProduct(apples, 1.99)
+    }
+
     @Test
     fun twoNormalItems() {
-        val catalog: SupermarketCatalog = FakeCatalog()
-        val toothbrush = Product("toothbrush", ProductUnit.EACH)
-        catalog.addProduct(toothbrush, 0.99)
-        val rice = Product("rice", ProductUnit.EACH)
-        catalog.addProduct(rice, 2.99)
-        val teller = Teller(catalog)
-        val cart = ShoppingCart()
-
-        // ACT
         cart.addItem(toothbrush)
         cart.addItem(rice)
+
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // ASSERT
         assertEquals(3.98, receipt.getTotalPrice(), 0.01)
     }
 
-
     @Test
     fun buyTwoGetOneFree() {
-        val catalog: SupermarketCatalog = FakeCatalog()
-        val toothbrush = Product("toothbrush", ProductUnit.EACH)
-        catalog.addProduct(toothbrush, 0.99)
-        val teller = Teller(catalog)
-        val cart = ShoppingCart()
-
-        // ACT
         cart.addItem(toothbrush)
         cart.addItem(toothbrush)
         cart.addItem(toothbrush)
         teller.addSpecialOffer(SpecialOfferType.THREE_FOR_TWO, toothbrush, catalog.getUnitPrice(toothbrush))
+
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // ASSERT
         assertEquals(1.98, receipt.getTotalPrice(), 0.01)
     }
 
     @Test
     fun xForYDiscount() {
-        val catalog: SupermarketCatalog = FakeCatalog()
-        val cherryTomatoes = Product("cherry Tomato box", ProductUnit.EACH)
-        catalog.addProduct(cherryTomatoes, 0.69)
-        val teller = Teller(catalog)
-        val cart = ShoppingCart()
-
-        // ACT
         cart.addItem(cherryTomatoes)
         cart.addItem(cherryTomatoes)
         teller.addSpecialOffer(SpecialOfferType.TWO_FOR_AMOUNT, cherryTomatoes, 0.99)
+
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // ASSERT
         assertEquals(0.99, receipt.getTotalPrice(), 0.01)
     }
 
     @Test
     fun FiveForYDiscount() {
-        val catalog: SupermarketCatalog = FakeCatalog()
-        val apples = Product("apples", ProductUnit.KILO)
-        catalog.addProduct(apples, 1.99)
-        val teller = Teller(catalog)
-        val cart = ShoppingCart()
-
-        // ACT
         cart.addItemQuantity(apples, 5.0)
         teller.addSpecialOffer(SpecialOfferType.FIVE_FOR_AMOUNT, apples, 6.99)
+
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // ASSERT
         assertEquals(6.99, receipt.getTotalPrice(), 0.01)
     }
 }
